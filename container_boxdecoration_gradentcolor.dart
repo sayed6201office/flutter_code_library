f562ba2a248c18f2fff520162427caf6b34d0cff
unknown <sayed.ahmed6662@gmail.com>
2019-12-18 18:37:34 +0600

    18 dec web by sayed office

--- container_boxdecoration_gradentcolor.dart
+++ container_boxdecoration_gradentcolor.dart
@@ -1,5 +1,6 @@
 =============================================================================================
-Alert Dialog Flutter, one button
+Container: BoxDecoration : LinearGradient
+Gradinrt color display
 =============================================================================================
 Container(
       padding: const EdgeInsets.all(15),
@@ -7,6 +8,7 @@ Container(
         title,
         style: Theme.of(context).textTheme.title,
       ),
+      //display gradient color....
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
@@ -21,3 +23,41 @@ Container(
     );
 
   
+=============================================================================================
+Text: softWrap: overflow
+=============================================================================================
+  Container(
+                    width: 300,
+                    //setting container bg color transperent......
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      //making text wrap, fade if its too big
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+ 
+=============================================================================================
+Container
+=============================================================================================
+Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 150,
+      width: 300,
+      child: child,
+    );
---
+++ stack.dart
@@ -0,0 +1,101 @@
+
+/*
+===========================================================================================
+Local app cardview design
+img:local
+===========================================================================================
+*/
+Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            //Stack: to position and display item on top of each other:::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            Stack(
+              children: <Widget>[
+                //ClipRRect: for image display and radius cropping:::::::::::::::::::::::::::::::::::::::::::::::::::::::
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                //Positioned: only available inside stack :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    //osetting container bg color transperent......
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.schedule,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.work,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.attach_money,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
