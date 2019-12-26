2beec090b46ec24a61de03d07991ecc4d391aa72
sayed6201datahost <sayed@datahost.com>
2019-12-26 18:56:56 +0600

    thu 26 dec by sayed

--- alert_dialog.dart
+++ alert_dialog.dart
@@ -49,3 +49,35 @@ showAlertDialog(BuildContext context) {
     },
   );
 }
+
+=============================================================================================
+Stateful bottomsheet
+=============================================================================================
+showDialog(
+  context: context,
+  builder: (context) {
+    String contentText = "Content of Dialog";
+    return StatefulBuilder(
+      builder: (context, setState) {
+        return AlertDialog(
+          title: Text("Title of Dialog"),
+          content: Text(contentText),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("Cancel"),
+            ),
+            FlatButton(
+              onPressed: () {
+                setState(() {
+                  contentText = "Changed Content of Dialog";
+                });
+              },
+              child: Text("Change"),
+            ),
+          ],
+        );
+      },
+    );
+  },
+);
