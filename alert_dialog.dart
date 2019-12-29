9134ab81b57dc9a5f75e596574c16562025939e5
sayed6201datahost <sayed@datahost.com>
2019-12-29 18:51:29 +0600

    29 dec by sayed office

--- alert_dialog.dart
+++ alert_dialog.dart
@@ -50,6 +50,59 @@ showAlertDialog(BuildContext context) {
   );
 }
 
+
+=============================================================================================
+stateful Alert dialog 
+=============================================================================================
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: RaisedButton(
+      child: Text('Open Dialog'),
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (_) {
+              return MyDialog();
+            });
+      },
+    )));
+  }
+}
+
+class MyDialog extends StatefulWidget {
+  @override
+  _MyDialogState createState() => new _MyDialogState();
+}
+
+class _MyDialogState extends State<MyDialog> {
+  Color _c = Colors.redAccent;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Container(
+        color: _c,
+        height: 20.0,
+        width: 20.0,
+      ),
+      actions: <Widget>[
+        FlatButton(
+            child: Text('Switch'),
+            onPressed: () => setState(() {
+                  _c == Colors.redAccent
+                      ? _c = Colors.blueAccent
+                      : _c = Colors.redAccent;
+                }))
+      ],
+    );
+  }
+}
+
 =============================================================================================
 Stateful bottomsheet
 =============================================================================================
